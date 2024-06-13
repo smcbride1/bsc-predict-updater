79e8106eecb9f971ccc828df83d4a1b05231c72f
Stephen McBride <stevemcbride3@gmail.com>
2024-06-13 00:59:28 -0400

    Added loop that auto updates every interval

--- update.py
+++ update.py
@@ -3,15 +3,25 @@ from update_lottery import update_lottery
 from update_predict import PredictGame, update_predict
 import os
 import datetime as dt
+import time
 
 base_path = os.path.dirname(os.path.realpath(__file__))
 
 def commit():
   subprocess.call(['sh', f'{base_path}/update-git.sh'])
 
+delay = 600
 if __name__ == "__main__":
   # update(config=testnet, update_all=False)
-  update_predict(game=PredictGame.PS_MAINNET, update_all=False)
-  update_predict(game=PredictGame.PRDT_MAINNET, update_all=False)
-  update_lottery(update_all=False)
-  commit()
+  while True:
+    print("Updating PS_MAINNET...")
+    update_predict(game=PredictGame.PS_MAINNET, update_all=False)
+    print("Updating PRDT_MAINNET...")
+    update_predict(game=PredictGame.PRDT_MAINNET, update_all=False)
+    print("Updating lottery...")
+    update_lottery(update_all=False)
+    print("Updating commit...")
+    commit()
+    print(f"Finished updates, waiting {delay} seconds...")
+    time.sleep(delay)
+    
